89329a0186e4a5fa0ce30c4445f2492b9c8c1903
TheUnit55 <123398760+TheUnit55@users.noreply.github.com>
2023-01-25 00:53:02 -0500

    Update Picture.java

--- Picture.java
+++ Picture.java
@@ -11,21 +11,29 @@
  */
 public class Picture 
 {
-    private Square wall;
-    private Square window;
-    private Triangle roof;
     private Circle sun;
+    private Square base1;
+    private Square base2;
+    private Square base3;
+    private Square base4;
+    private Square base5;
+    private Triangle corner1;
     private boolean drawn;
+    
+    
 
     /**
      * Constructor for objects of class Picture
      */
     public Picture()
     {
-        wall = new Square();
-        window = new Square();
-        roof = new Triangle();
         sun = new Circle();
+        base1 = new Square();
+        base2 = new Square();
+        base3 = new Square();
+        base4 = new Square();
+        base5 = new Square();
+        corner1 = new Triangle();
         drawn = false;
     }
 
@@ -35,27 +43,48 @@ public class Picture
     public void draw()
     {
         if(!drawn) {
-            wall.moveHorizontal(-140);
-            wall.moveVertical(20);
-            wall.changeSize(120);
-            wall.makeVisible();
-            
-            window.changeColor("black");
-            window.moveHorizontal(-120);
-            window.moveVertical(40);
-            window.changeSize(40);
-            window.makeVisible();
-    
-            roof.changeSize(60, 180);
-            roof.moveHorizontal(20);
-            roof.moveVertical(-60);
-            roof.makeVisible();
-    
-            sun.changeColor("yellow");
-            sun.moveHorizontal(100);
-            sun.moveVertical(-40);
-            sun.changeSize(80);
+            sun.changeColor("magenta");
+            sun.moveHorizontal(-90);
+            sun.moveVertical(-33);
+            sun.changeSize(180);
             sun.makeVisible();
+            
+            base1.moveHorizontal(-100);
+            base1.moveVertical(100);
+            base1.changeSize(40);
+            base1.makeVisible();
+            base1.changeColor("black");
+            
+            base2.moveHorizontal(-100);
+            base2.moveVertical(60);
+            base2.changeSize(40);
+            base2.makeVisible();
+            base2.changeColor("black");
+            
+            base3.moveHorizontal(-100);
+            base3.moveVertical(20);
+            base3.changeSize(40);
+            base3.makeVisible();
+            base3.changeColor("black");
+            
+            base4.moveHorizontal(-100);
+            base4.moveVertical(-20);
+            base4.changeSize(40);
+            base4.makeVisible();
+            base4.changeColor("black");
+            
+            base5.moveHorizontal(-100);
+            base5.moveVertical(-60);
+            base5.changeSize(40);
+            base5.makeVisible();
+            base5.changeColor("black");
+    
+            corner1.changeSize(10, 100);
+            corner1.moveHorizontal(-50);
+            corner1.moveVertical(-70);
+            corner1.makeVisible();
+            corner1.changeColor("black");
+    
             drawn = true;
         }
     }
@@ -65,10 +94,11 @@ public class Picture
      */
     public void setBlackAndWhite()
     {
-        wall.changeColor("black");
-        window.changeColor("white");
-        roof.changeColor("black");
         sun.changeColor("black");
+        base1.changeColor("white");
+        base2.changeColor("white");
+        base3.changeColor("white");
+        corner1.changeColor("black");
     }
 
     /**
@@ -76,9 +106,10 @@ public class Picture
      */
     public void setColor()
     {
-        wall.changeColor("red");
-        window.changeColor("black");
-        roof.changeColor("green");
         sun.changeColor("yellow");
+        base1.changeColor("black");
+        base2.changeColor("black");
+        base3.changeColor("black");
+        corner1.changeColor("green");
     }
 }
